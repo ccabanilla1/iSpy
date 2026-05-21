e6a9fcbcf8aa8b1465e16aa3ad08e222b0678bf3
Chantelle Cabanilla <chanillie11@gmail.com>
2026-05-20 17:36:38 -0700

    added boxes!

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import cv2
+import random
+from ultralytics import YOLO
+
+model = YOLO("yolov8n.pt")  # get the AI brain ready
+
 
 cam = cv2.VideoCapture(0)
 cv2.namedWindow("iSpy", cv2.WINDOW_NORMAL)
@@ -9,10 +14,16 @@ while True:
     if not ret:
         print("Could not read Camera frame")
         break
-
-    cv2.imshow("iSpy", frame)
+    
+    results = model(frame, verbose=False)
+    
+    annotated_frame = results[0].plot()
+    cv2.imshow("iSpy", annotated_frame)
+    
+    result = results[0]
+    
     if cv2.waitKey(20) & 0xFF == ord("q"):
         break
-    
+
 cam.release()
 cv2.destroyAllWindows()
